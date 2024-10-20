948e81e8db07b4db39bbe2323ba3f6f10811272f
thiagoeu <araujo.thiago1051@gmail.com>
2024-10-20 11:20:53 -0300

    feat: task_edit

--- tasks/views.py
+++ tasks/views.py
@@ -1,17 +1,22 @@
 from django.shortcuts import render
-from .models import Task
 from django.views.generic import ListView
+from django.views.generic.edit import UpdateView
+from django.urls import reverse_lazy
 
-
-
-# Create your views here.
-
-# def task_list(request):
-#     tasks = Task.objects.all()
-#     return render(request, 'tasks/task_list.html',{'tasks' : tasks})
+from .forms import TaskForm
+from .models import Task
 
 
 class TaskListView(ListView):
     model = Task
     template_name = 'tasks/task_list.html'
     context_object_name = 'tasks'
+
+class TaskUpdateView(UpdateView):
+    model = Task
+    form_class = TaskForm
+    template_name = 'tasks/task_edit.html'
+    success_url = reverse_lazy('task-list')
+    def form_valid(self, form):
+        return super().form_valid(form)
+    
