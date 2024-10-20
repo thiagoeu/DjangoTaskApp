81c7efed26e6b645cb0b0bebe8c391f59d033c0a
thiagoeu <araujo.thiago1051@gmail.com>
2024-10-20 10:36:38 -0300

    feat: task_list viwer

--- setup/urls.py
+++ setup/urls.py
@@ -16,7 +16,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from tasks.views import TaskListView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('',TaskListView.as_view(), name='task-list')
 ]
--- tasks/views.py
+++ tasks/views.py
@@ -1,10 +1,17 @@
 from django.shortcuts import render
 from .models import Task
+from django.views.generic import ListView
 
 
 
 # Create your views here.
 
-def task_list(request):
-    tasks = Task.objects.all()
-    return render(request, 'tasks/task_list.html',{'tasks' : tasks})
+# def task_list(request):
+#     tasks = Task.objects.all()
+#     return render(request, 'tasks/task_list.html',{'tasks' : tasks})
+
+
+class TaskListView(ListView):
+    model = Task
+    template_name = 'tasks/task_list.html'
+    context_object_name = 'tasks'
